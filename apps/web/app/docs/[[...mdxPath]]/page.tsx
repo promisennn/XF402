70114ebce0b8a62a8c00b604b2c7c4f8861f1916
promisennn <promisethedev@gmail.com>
2025-11-02 02:17:12 +0400

    move mdx-components

--- apps/web/app/docs/[[...mdxPath]]/page.tsx
+++ apps/web/app/docs/[[...mdxPath]]/page.tsx
@@ -1,5 +1,5 @@
 import { generateStaticParamsFor, importPage } from "nextra/pages";
-import { useMDXComponents as getMDXComponents } from "@/components/mdx-components";
+import { useMDXComponents as getMDXComponents } from "@/mdx-components";
 
 export const generateStaticParams = generateStaticParamsFor("mdxPath");
 
