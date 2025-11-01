cbacb6bd4b19d8a43c729e6d4f58228e2a6d8923
promisennn <promisethedev@gmail.com>
2025-11-02 02:29:06 +0400

    test no alias

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/hero/Hero";
-import { Carousel } from "@/components/carousel/Carousel";
+import { Carousel } from "../components/carousel/Carousel";
 export default function Home() {
   return (
     <div className="min-h-screen items-center justify-center font-sans">
